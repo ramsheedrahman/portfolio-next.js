c93a56eda47544e1fcf5d232cfb9035152556772
Ramsheed K K <ramsheedkk06@gmail.com>
2024-03-20 21:08:11 +0530

    navbar modified

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
         <div className="menu hidden md:block md:w-auto id=navbar ">
             <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8'>
             {navLinks.map((link, index) => (
-              <li key={index}>
+              <li  key={index}>
                 <NavLink href={link.path} title={link.title} />
               </li>
             ))}
